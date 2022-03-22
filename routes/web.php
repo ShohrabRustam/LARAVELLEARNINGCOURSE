ae20e9417de2a31490016d4883c9585d95af94eb
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-22 23:40:39 +0530

    make resource route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LearningController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//call with learning/create
+Route::resource('learning',LearningController::class);
